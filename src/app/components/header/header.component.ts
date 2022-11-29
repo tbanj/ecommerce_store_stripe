9fd7f3c1f33bceba58c968c7a9fe34c3cbc842fe
Alabi Temitope <t.banji@rocketmail.com>
2022-11-29 10:33:44 +0100

    tailwind integrated

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  template: `
+    <p>
+      header works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class HeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
