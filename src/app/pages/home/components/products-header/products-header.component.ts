72c019ecee240cae80824bfda1d5e5cb45a6333e
Alabi Temitope <t.banji@rocketmail.com>
2022-11-29 14:23:38 +0100

    home  header component

---
+++ src/app/pages/home/components/products-header/products-header.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-products-header',
+  template: `
+    <p>
+      products-header works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class ProductsHeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
