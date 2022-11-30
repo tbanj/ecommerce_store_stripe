86503c4b0efbef85d9f679652a40696122180441
Alabi Temitope <t.banji@rocketmail.com>
2022-11-30 01:38:09 +0100

    event from header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,6 +21,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ProductsHeaderComponent } from './pages/home/components/products-header/products-header.component';
 import { FiltersComponent } from './pages/home/components/filters/filters.component';
+import { ProductBoxComponent } from './pages/home/components/product-box/product-box.component';
 
 @NgModule({
   declarations: [
@@ -28,7 +29,8 @@ import { FiltersComponent } from './pages/home/components/filters/filters.compon
     HeaderComponent,
     HomeComponent,
     ProductsHeaderComponent,
-    FiltersComponent
+    FiltersComponent,
+    ProductBoxComponent
   ],
   imports: [
     BrowserModule,
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+const ROWS_HEIGHT: {[id: number]: number} = {1: 400, 3: 335, 4: 350};
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html'
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   cols= 3;
+  rowHeight = ROWS_HEIGHT[this.cols];
   category: string | undefined;
 
   constructor() { }
@@ -16,6 +18,7 @@ export class HomeComponent implements OnInit {
 
   onColumnsCountChange(colsNum: number): void {
     this.cols =  colsNum;
+    this.rowHeight = ROWS_HEIGHT[this.cols]; 
   }
 
   onShowCategory(newCategory: string): void{
