40a6337011f968108df3ad503752de90de9356b6
Alabi Temitope <t.banji@rocketmail.com>
2022-11-30 02:40:33 +0100

    cartItem implemented

---
+++ src/app/pages/cart/cart.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cart',
+  template: `
+    <p>
+      cart works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class CartComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
