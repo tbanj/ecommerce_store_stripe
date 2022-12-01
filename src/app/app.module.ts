11d252d661a7df799464ed6138d6e4318746fc63
Alabi Temitope <t.banji@rocketmail.com>
2022-12-01 08:06:57 +0100

    cart page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import  { MatSidenavModule } from '@angular/material/sidenav';
-import {MatGridListModule } from '@angular/material/grid-list';
-import {MatMenuModule } from '@angular/material/menu';
-import {MatButtonModule } from '@angular/material/button';
-import {MatCardModule } from '@angular/material/card';
-import {MatIconModule } from '@angular/material/icon';
-import {MatListModule } from '@angular/material/list';
-import {MatToolbarModule } from '@angular/material/toolbar';
-import {MatExpansionModule } from '@angular/material/expansion';
-import {MatTableModule } from '@angular/material/table';
-import {MatBadgeModule } from '@angular/material/badge';
-import {MatSnackBarModule } from '@angular/material/snack-bar';
+import {  MatGridListModule } from '@angular/material/grid-list';
+import {  MatMenuModule } from '@angular/material/menu';
+import {  MatButtonModule } from '@angular/material/button';
+import {  MatCardModule } from '@angular/material/card';
+import {  MatIconModule } from '@angular/material/icon';
+import {  MatListModule } from '@angular/material/list';
+import {  MatToolbarModule } from '@angular/material/toolbar';
+import {  MatExpansionModule } from '@angular/material/expansion';
+import {  MatTableModule } from '@angular/material/table';
+import {  MatBadgeModule } from '@angular/material/badge';
+import {  MatSnackBarModule } from '@angular/material/snack-bar';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -1,20 +1,39 @@
+import { CartItem } from './../../models/cart.model';
 import { Component, OnInit } from '@angular/core';
+import { Cart } from 'src/app/models/cart.model';
 
 @Component({
   selector: 'app-cart',
-  template: `
-    <p>
-      cart works!
-    </p>
-  `,
-  styles: [
-  ]
+  templateUrl: './cart.component.html'
 })
 export class CartComponent implements OnInit {
+  cart: Cart = { items: [
+    {
+    product: 'https://via.placeholder.com/150',
+    name: 'snickers',
+    price: 150,
+    quantity: 1,
+    id: 1
+  }
+]};
 
+  dataSource: Array<CartItem> = [];
+  displayedColumns: Array<string>=[
+    'product',
+    /* 'name',
+    'price',
+    'quantity',
+    'total',
+    'action' */
+  ]
+
+  
+  
   constructor() { }
 
   ngOnInit(): void {
+    this.dataSource = this.cart.items;
+    console.log('cart', this.cart);
   }
 
 }
