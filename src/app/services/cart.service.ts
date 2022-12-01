5b1eeb5533dbe4ab30a3728ad1edd49da7460f83
Alabi Temitope <t.banji@rocketmail.com>
2022-12-01 16:42:21 +0100

    product model progress

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -1,9 +1,36 @@
 import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { BehaviorSubject } from 'rxjs';
+import { Cart, CartItem } from '../models/cart.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
+  cart = new BehaviorSubject<Cart>({items: []});
+  constructor(private _snackBar: MatSnackBar) { }
 
-  constructor() { }
+  addToCart(item: CartItem): void {
+    const items =[...this.cart.value.items];
+
+    const itemInCart = items.find((_item) => _item.id === item.id);
+
+    if (itemInCart) {
+      itemInCart.quantity += 1;
+    } else {
+      items.push(item);
+    }
+
+    this.cart.next({ items});
+    this._snackBar.open('Item added to cart', 'Ok', { duration: 3000 });
+    console.log(this.cart.value);
+    
+    
+  }
+
+  getTotal(items: Array<CartItem>): number {
+    return items
+      .map((item)=> item.price * item.quantity)
+      .reduce((prev, current)=> prev+ current, 0)
+  }
 }
